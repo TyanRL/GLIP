ba6d9f628e9f88d8a98be3f8734b9b7771788636
Marcus Wright <marcus03wright@icloud.com>
2025-04-12 22:48:45 -0400

    Updating environment for compatability

--- create_env.sh
+++ create_env.sh
@@ -1,9 +1,9 @@
 conda env create -f env.yaml
 
-pip install torch==1.9.1+cu111 torchvision==0.10.1+cu111 torchaudio==0.9.1 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 --index-url https://download.pytorch.org/whl/cu121
 
 pip install -r requirements.txt
 
-pip install "numpy<2.0"
-
 python3 install_nltk_downloads.py 
+
+python setup.py build develop --user
